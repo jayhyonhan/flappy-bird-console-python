9e38b3362753464370348804f8dec6c87a822fa1
jayhyonhan <jayhyonhan@gmail.com>
2023-10-13 16:33:35 +0100

    hopefully there are no bugs...

--- main.py
+++ main.py
@@ -1,32 +1,71 @@
-import t_matrix, sys, time, keyboard
+import t_matrix, sys, time, keyboard, random # t_matrix is a custom module that creates a matrix type and it makes generating the level much more easier (Read it!)
 from os import system, name
 width = 100
 height = 50
 level_background = t_matrix.matrix(width, height, " ")
 level = level_background
-bird = [25, 10, 0.1] # [y(inverted), x]
+bird = [25, 20, 0.1] # [y(inverted), x]
+jump_pressed = False
+pipe = [0, 1] # [height, x]
+pipe[0] = random.randint(1, height-height//4)
+score = 0
 def clear():
     if name == 'nt':
         _ = system('cls')
     else:
         _ = system('clear')
 while True:
-    if keyboard.is_pressed(" "):
-        bird[2] = -1
+    if pipe[1] <= 1:
+        pipe[0] = random.randint(1, height-height//4)
+        pipe[1] = width
+    else:
+        pipe[1] -= 1
+    if (jump_pressed == True) and not(keyboard.is_pressed("f")): # press f to jump
+        jump_pressed = False
+    elif keyboard.is_pressed("f") and not(jump_pressed):
+        jump_pressed = True
+        bird[2] = -1.3
     bird[0] += bird[2]
     if bird[0]>len(level._mat):
         clear()
         print("Game Over")
+        print(f"Score: {score}")
         exit()
     elif bird[0]<=0:
         clear()
         print("Game Over")
+        print(f"Score: {score}")
         exit()
-    bird[2] += 0.1 # gravity acceleration
-    if bird[2] > 1:
-        bird[2] = 1
+    bird[2] += 0.2 # gravity acceleration
+    if bird[2] > 1.3:
+        bird[2] = 1.3
     level.reset_mat()
-    level._mat[int(bird[0])][bird[1]] = "B"
+    for i in range(0, pipe[0]+1):
+        level._mat[i][pipe[1]-1] = "#"
+        if pipe[1] > 1:
+            level._mat[i][pipe[1]-2] = "#"
+        if pipe[1] > 2:
+            level._mat[i][pipe[1]-3] = "#"
+    for j in range((pipe[0]+height//4), height):
+        level._mat[j][pipe[1]-1] = "#"
+        if pipe[1] > 1:
+            level._mat[j][pipe[1]-2] = "#"
+        if pipe[1] > 2:
+            level._mat[j][pipe[1]-3] = "#"
+    level._mat[int(bird[0])][bird[1]-1] = "B" # "drawing" the bird (yes I am using a B for the bird)
+    if (pipe[1]-2) <= bird[1] <= (pipe[1]):
+        if pipe[0] >= bird[0]:
+            clear()
+            print("Game Over")
+            print(f"Score: {score}")
+            exit()
+        elif bird[0] >= (pipe[0]+height//4):
+            clear()
+            print("Game Over")
+            print(f"Score: {score}")
+            exit()
+    if pipe[1] == bird[1]:
+        score += 1
     output = ""
     for i in range(level.h+2):
         if i == 0:
@@ -45,8 +84,10 @@ while True:
                         output += " "
         if i < level.h+1:
             output += "\n"
+    output += f"\n\n{score}"
     sys.stdout.flush()
-    time.sleep(0.025)
+    time.sleep(0.05)
     clear()
     print(output)
+    # replaced with line 33
     #level._mat[int(bird[0])][bird[1]] = " "
--- t_matrix.py
+++ t_matrix.py
@@ -1,14 +1,14 @@
 class matrix:
-	def __init__(self, width, height, fill):
-		self.w = width
-		self.h = height
+	def __init__(self, width, height, fill): # fill is the character that is going to fill the matrix
+		self.w = width # width
+		self.h = height # height
 		self.fill = fill
-		self._mat = []
-		for i in range(self.h):
+		self._mat = [] # i dunno why I named it _mat
+		for i in range(self.h): # creating the matrix
 			self._mat.append([])
 			for j in range(self.w):
 				self._mat[i].append(self.fill)
-	def reset_mat(self):
+	def reset_mat(self): # resetting the matrix
 		self._mat = []
 		for i in range(self.h):
 			self._mat.append([])
